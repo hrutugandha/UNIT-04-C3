951d2b66069086fa3daecd63e13e3f48036d63e3
BishamontenSama <62648846+hrutugandha@users.noreply.github.com>
2022-03-21 10:51:23 +0530

    edited route

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const connect = require('./config/db')
 
-const start = () =>{
+const start = async() =>{
     await connect;
     app.listen(3000, ()=>{
         console.log('listening on port 3000');
